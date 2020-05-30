92c2de03dd4a26e7d23d98f95d339eb62da6f531
WSong <wsong0101@gmail.com>
2020-05-30 19:39:28 +0900

    user service에 handleResponse 빠진것 추가

--- js/service/user.service.js
+++ js/service/user.service.js
@@ -46,3 +46,20 @@ function register(user) {
 
     return fetch(`${config.apiUrl}/register`, requestOptions).then(handleResponse);
 }
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                logout();
+                location.reload(true);
+            }
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
